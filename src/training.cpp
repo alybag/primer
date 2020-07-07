02002c012d86d55b4186aecacbb7b0824862f3bc
alybag <alybag@byu.edu>
2020-07-07 00:51:37 -0600

    Inital commit

---
+++ src/training.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+double expliciteuler(int n, double a, double b, double initial, ostream& output)
+{
+	//local variables
+	double h, x, w; //h=dt, x=t, w=x, a=0, b=9
+
+	//initialize values
+	h = (b-a)/(double)(n);
+	x = a;
+	w = initial;
+
+	//Euler's method
+	for (int i=0; i<n; i++)
+	{
+		x = a + i*h; //update value of x to x_i
+		output << x << " " << w << endl; //output x_i and w_i to screen
+		w = w + h*(-3*x); //update w to w_{i+1}
+	}
+	output << b << " " << w << endl;
+	return w;
+}
+
+int main()
+{
+	//read in params.dat
+	string line;
+	ifstream infile ("params.dat");
+	if (infile.is_open())
+	{
+		while ( getline (infile,line) )
+		{
+			cout << line << '\n';
+		}
+		infile.close();
+	}
+	else cout << "Unable to open file";
+
+	//output to output.dat
+	ofstream myFileStream("output.dat");
+	expliciteuler(9, 0., 9., 1., myFileStream);
+	myFileStream.close();
+
+	return 0;
+}
